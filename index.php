4531e0d64f0a1ec2b25303f25c6c64233071ae7f
hung09 <94339728+NguyenVanHung09102001@users.noreply.github.com>
2022-01-11 16:33:07 +0700

    btgk

--- index.php
+++ index.php
@@ -1,48 +1,38 @@
 <?php
-    require "view/template/header.php";
-?>
+    // 1. index.php là TRANG MẶC ĐỊNH sẽ CHẠY ĐẦU TIÊN
+    // Nhiệm vụ trong mô hình này: Nó chỉ đơn giản phân tích xem người dùng đang MUỐN GÌ để CHUYỂN TIẾP công việc tới CHỨC NĂNG tương ứng
+    // Coi mọi URL của người dùng có dạng: http://localhost/project_mvc/index.php?controller=user&action=index
+    // Để tôi xem anh muốn ĐỐI TƯỢNG NÀO (controller là user hay employee, hay unit) và THỰC HIỆN HÀNH ĐỘNG GÌ (index, delete, insert, update)
+    // lấy ra controller
+    $controller = isset($_GET['controller']) ? $_GET['controller'] : 'user';
+    //lấy ra action
+    $action     = isset($_GET['action']) ? $_GET['action'] : 'index';
+    // Trường hợp đặc biệt  là controller và action ko xuất hiện trên URL, nó mặc định là Hiển thị Danh sách người dùng
+    // http://localhost/project_mvc/index.php => http://localhost/project_mvc/index.php?controller=user&action=index
 
-    <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div>
-                        <a class="btn btn-primary" href="index.php?controller=employee&action=index">Chi tiết</a>
-                    </div>
-                    <table class="table">
-                        <thead>
-                          <tr>
-                            <th scope="col">Mã độc giả </th>
-                            <th scope="col">Họ tên</th>
-                            <th scope="col">Gioi tinh</th>
-                            <th scope="col">NĂM SINH</th>
-                            <th scope="col">ngaycapthe</th>
-                            <th scope="col">ngayhethan</th>
-                            <th scope="col">diachi</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                            foreach($users as $row){
-                        ?>
-                            <tr>
-                                <th scope="row"><?php echo $row['madg'];?></th>
-                                <td><?php echo $row['hovaten']; ?></td>
-                                <td><?php echo $row['gioitinh']; ?></td>
-                                <td><?php echo $row['namsinh']; ?></td>
-                                <td><?php echo $row['ngaycapthe']; ?></td>
-                                <td><?php echo $row['ngayhethan']; ?></td>
-                                <td><?php echo $row['diachi']; ?></td>
-                            </tr>
-                        <?php
-                            }
-                        ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </main>
-<?php
-    require "view/template/footer.php";
+    $controller = ucfirst($controller); //URL của chúng ta thiết kế luôn có dạng chữ thường > ucfirst = upper case first => Chuyển kí tự đầu tiên
+    // sang chữ IN HOA
+    $fileController = $controller . "Controller.php"; //Tên tệp tin muốn truy xuất
+
+    $pathController = "controller/$fileController"; //Đường dẫn đầy đủ tới tệp tin Controller muốn truy xuất
+
+    if (!file_exists($pathController)) {
+        die("Trang bạn tìm không tồn tại"); //Nếu đoạn này xảy ra, chương trình dừng thực hiện
+    }
+
+    // Nếu ko, gộp tệp tin Controller tương ứng vào đây
+    require_once "$pathController";
+
+    $classController = $controller."Controller"; //Xác định tên Class trong Controller đang định nghĩa
+
+    $object = new $classController(); //Tạo ra đối tượng tương ứng với Class vừa xác định ở trên
+
+    if (!method_exists($object, $action)) {
+        die("Phương thức $action không tồn tại trong class $classController"); //Kiểm tra action có tồn tại trong Controller ko
+    }
+    // Nếu có thì gọi action tương ứng tên phương thức
+    $object->$action();
+
+
+
 ?>
